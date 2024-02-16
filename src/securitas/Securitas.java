6a5a8fac291fa9f7a57376a9e510cd11536fa37c
Fernando Delgado Cruz <ferd9800@gmail.com>
2024-02-16 15:09:55 -0600

    Programa Base

---
+++ src/securitas/Securitas.java
@@ -0,0 +1,17 @@
+
+package securitas;
+
+/**
+ *
+ * @author Programadores-Skynet
+ */
+public class Securitas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
